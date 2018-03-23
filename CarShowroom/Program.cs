53d3d0017f1029bda3855e8a78a97c65c7e3da03
Yu-Jin <yujin_1991@hotmail.com>
2018-03-23 11:01:54 +0000

    user can enter car details and view them

---
+++ CarShowroom/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CarShowroom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> carlist  = new List<Car>();
+            string userInput = "";
+            do
+            {
+                Console.WriteLine($"Enter 'add' to add house, 'show' to see car list or 'exit': ");
+                userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "add":
+                        Console.WriteLine("Enter Car Model:");
+                        string carModel = Console.ReadLine();
+
+                        Console.WriteLine("Enter Car Color:");
+                        string carColor = Console.ReadLine();
+
+                        Console.WriteLine("Enter Car Price:");
+                        int carPrice = Convert.ToInt32(Console.ReadLine());
+
+                        Car carObject = new Car(carModel, carColor, carPrice);
+                        carlist.Add(carObject);
+                        break;
+                    case "show":
+                        foreach(Car car in carlist)
+                            Console.WriteLine("{0} {1} £{2:N}", car.color, car.model, car.price);
+                        break;
+                    case "exit":
+                        break;
+                }
+            }
+            while (userInput != "exit");
+        }
+    }
+
+    public class Car{
+        public static int totalNumberCars = 0;
+        public string model;
+        public string color;
+        public int price;
+        public bool sold;
+
+        public Car(string model, string color, int price)
+        {
+            this.model = model;
+            this.color = color;
+            this.price = price;
+            this.sold = false;
+            Car.totalNumberCars++;
+        }
+
+        public Car()
+        {
+            Car.totalNumberCars++;
+        }
+
+
+    }
+}
